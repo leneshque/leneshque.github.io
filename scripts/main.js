bf3cee28ea9a1eee4b2b2f0da650f9232a6b47af
leneshque <155529157+leneshque@users.noreply.github.com>
2024-01-03 17:15:02 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,56 @@
+const myImage = document.querySelector("img");
+var counter = 1;
+
+myImage.onclick = function(){
+switch(counter){
+    case 1:
+        myImage.setAttribute("src","images/aaa.png");
+        myImage.setAttribute("alt","a banana taped onto the wall");
+        break;
+    case 2:
+        myImage.setAttribute("src","images/bbb.jpg");
+        myImage.setAttribute("alt", "Can't help myself - a robot art that conveys emotion");
+        break;
+    case 3:
+        myImage.setAttribute("src","images/ccc.jpeg");
+        myImage.setAttribute("alt","Van Gogh's painting - Wheat Field with Cypresses");
+        break;
+}
+counter++;
+if (counter>3) 
+    counter = 0;
+}
+
+/*
+myImage.onclick = function() {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/aaa.png"){
+        myImage.setAttribute("src","images/bbb.jpg")
+    }else{
+    myImage.setAttribute("src","images/aaa.png");
+    }
+};
+*/
+
+let myButton=document.querySelector("button");
+let myHeader=document.querySelector("h1");
+
+if (localStorage.getItem("name")){
+    const storedName = localStorage.getItem("name");
+    myHeader.textContent=`Hello, ${storedName}!`;
+}
+
+function setUserName(){
+    const myName = prompt("Please enter your name.");
+    if (myName){
+    localStorage.setItem("name",myName);
+    myHeader.textContent = `Hello, ${myName}!`;
+    }
+    else{
+        setUserName();
+    }
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
